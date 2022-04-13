98e70492209c4c44ba0c905cfbbcdeb180a67755
swc0620 <pinetreewave@naver.com>
2022-04-13 21:35:49 +0900

    implemented MockDAIToken contract test

---
+++ test/MockDAIToken.test.ts
@@ -0,0 +1,38 @@
+import { waffle } from "hardhat";
+import { expect } from "chai";
+import { BigNumber, utils } from "ethers";
+
+import MockDAITokenArtifact from "../artifacts/contracts/MockDAIToken.sol/MockDAIToken.json";
+import { MockDAIToken } from "../typechain-types/contracts/MockDAIToken";
+
+const { deployContract } = waffle;
+
+describe("MockDAIToken", () => {
+    let mockDAIToken: MockDAIToken;
+
+    const [admin, backer1, backer2] = waffle.provider.getWallets();
+
+    beforeEach(async () => {
+        mockDAIToken = await deployContract(
+            admin,
+            MockDAITokenArtifact,
+            [
+                "MockDAIToken",
+                "MDAI"
+            ]
+        ) as MockDAIToken;
+    });
+
+    context('when MockDAIToken has been minted', async () => {
+        it('has given data', async () => {
+            expect(await mockDAIToken.totalSupply()).to.be.equal(BigNumber.from(utils.parseEther("10000")));
+            expect(await mockDAIToken.name()).to.be.equal("MockDAIToken");
+            expect(await mockDAIToken.symbol()).to.be.equal("MDAI");
+        });
+
+        it('distributes MDAI via faucest', async () => {
+            await mockDAIToken.connect(backer1).faucet();
+            expect(await mockDAIToken.balanceOf(backer1.address)).to.be.equal(BigNumber.from(utils.parseEther("1")));
+        });
+    });
+});
