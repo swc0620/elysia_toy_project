f11f5a9e302e786251e377ede50b4f4dbd49cd3a
swc0620 <pinetreewave@naver.com>
2022-04-14 16:02:46 +0900

    implemented ProjectFactory contract test

---
+++ test/ProjectFactory.test.ts
@@ -0,0 +1,35 @@
+import { waffle } from "hardhat";
+import { expect } from "chai";
+import { BigNumber, utils } from "ethers";
+
+import ProjectFactoryArtifact from "../artifacts/contracts/ProjectFactory.sol/ProjectFactory.json"
+import { ProjectFactory } from "../typechain-types/contracts/ProjectFactory"
+
+const { deployContract } = waffle
+
+describe("ProjectFactory", () => {
+    let projectFactory: ProjectFactory;
+
+    const [proposer, manufacturer] = waffle.provider.getWallets();
+
+    beforeEach(async () => {
+        projectFactory = await deployContract(
+            proposer,
+            ProjectFactoryArtifact,
+            []
+        ) as ProjectFactory;
+    });
+
+    context("when new ProjectFactory is deployed", async() => {
+        
+        it('creates new Project on proposer\'s request', async () => {
+            await projectFactory.connect(proposer).createProject("New Battery", BigNumber.from(utils.parseEther("0.01")), manufacturer.address, 10);
+            expect(await projectFactory.createdProjects(0)).to.not.equal(0);
+        });
+
+        it('gets addresses of created projects', async () => {
+            await projectFactory.connect(proposer).createProject("New Battery", BigNumber.from(utils.parseEther("0.01")), manufacturer.address, 10);
+            expect(await projectFactory.createdProjects(0)).to.be.equal((await projectFactory.getProjects())[0]);
+        });
+    });
+});
