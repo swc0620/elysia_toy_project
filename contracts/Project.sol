ef7a073e6e4da44ceb46c98a8f831af60c605da3
swc0620 <pinetreewave@naver.com>
2022-04-14 16:03:59 +0900

    implemented startBacking function

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -7,10 +7,31 @@ contract Project {
     address public manufacturer;
     uint public minimumBacking;
 
-    constructor(address proposer_, string memory description_, uint minimumBacking_, address manufacturer_) {
+    struct DueTime {
+        bool open;
+        uint closeTime;
+    }
+
+    DueTime public backingTime;
+
+    constructor(address proposer_, string memory description_, uint minimumBacking_, address manufacturer_, uint backingDuration_) {
         proposer = proposer_;
         description = description_;
         minimumBacking = minimumBacking_;
         manufacturer = manufacturer_;
+
+        _startBacking(backingDuration_);
+    }
+
+    modifier isProposer() {
+        require(msg.sender == proposer, 'Not proposer');
+        _;
+    }
+
+    function _startBacking(uint backingDuration_) private {
+        require(backingTime.open == false);
+        
+        backingTime.open = true;
+        backingTime.closeTime = block.timestamp + backingDuration_;
     }
 }
